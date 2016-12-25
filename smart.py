7c22ed87a914c85b3506aaa5a03f6673946b0a73
alifanov <lifanov.a.v@gmail.com>
2016-12-25 19:19:13 +0300

    add update DB by adding new code items

--- smart.py
+++ smart.py
@@ -1,8 +1,10 @@
 import ast
 import time
+import pickle
 import os
 import re
 from exprs import exprs as exprs_db
+import itertools
 
 
 def get_heads(l):
@@ -161,7 +163,8 @@ class ASTPatternMatcher(object):
                 result += [self.get_common_expr(heads)]
         # print('Result: {}'.format(result))
         if tails[0]:
-            if tails[0][0] and self.check_in_list(tails[0][0], tails[1]) and isinstance(tails[0][0], list) and len(tails[0]) != len(
+            if tails[0][0] and self.check_in_list(tails[0][0], tails[1]) and isinstance(tails[0][0], list) and len(
+                    tails[0]) != len(
                     tails[1]):
                 tails[0].insert(0, [])
         # print('Tails: {}'.format(tails))
@@ -211,6 +214,7 @@ class CodeSearcher(object):
         return []
 
 
+#TODO: move to ExprDB
 class ExprSearcher(object):
     def __init__(self, db):
         self.db = db
@@ -241,8 +245,85 @@ class ExprSearcher(object):
         return result
 
 
+class ExprDB(object):
+    db_name = 'exprs.pkl'
+    db = []
+    matcher = None
+
+    def read_db(self):
+        data = open(self.db_name, 'rb').read()
+        if data:
+            self.db = pickle.loads(data)
+        else:
+            return []
+
+    def keys(self):
+        return [rec[0] for rec in self.db]
+
+    def save(self):
+        f = open(self.db_name, 'wb')
+        f.write(pickle.dumps(self.db))
+        f.close()
+
+    def __init__(self):
+        self.read_db()
+        self.matcher = ASTPatternMatcher()
+
+    def update(self, rec, e, code):
+        codes = rec[1] + [code,]
+        codes = list(set(codes))
+        new_rec = (e, codes)
+        self.db = [new_rec if r == rec else rec for r in self.db]
+        self.save()
+
+    def flatten(self, l):
+        res = []
+        for ll in l:
+            if isinstance(ll, list):
+                res.extend(self.flatten(ll))
+            else:
+                res.append(ll)
+        return res
+
+    def add(self, e, code):
+        if not self.db and self.db is not None:
+            self.db.append(
+                (e, [code,])
+            )
+        else:
+            q = self.query(e, fuzzy=True)
+            if q:
+                self.update(q[0], q[0][0], code)
+            else:
+                for rec in self.db:
+                    matched = self.matcher.get_common_expr([rec[0], e])
+                    _matched = self.flatten(matched)
+                    if not all([m == '*' for m in _matched]):
+                        if matched == e:
+                            self.update(rec, e, code)
+                        else:
+                            self.update(rec, matched, code)
+                    else:
+                        self.db.append(
+                            (e, [code,])
+                        )
+        self.save()
+
+    def query(self, q, fuzzy=False):
+        res = []
+        for rec in self.db:
+            if fuzzy:
+                if self.matcher.get_common_expr([q, rec[0]]) in [q, rec[0]]:
+                    res.append(rec)
+            else:
+                if rec == q:
+                    res.append(rec)
+        return res
+
+
 if __name__ == "__main__":
     BASE_DIR = 'codes/learn/'
+    edb = ExprDB()
     exprs = []
     codes = []
     for f in os.listdir(BASE_DIR):
@@ -250,11 +331,12 @@ if __name__ == "__main__":
         if os.path.isfile(path):
             code = open(path).read()
             ast_expr = ASTGenerator(code).parsed_ast
-            print('AST: ', ast_expr)
+            # print('AST: ', ast_expr)
             for ae in ast_expr:
+                edb.add(ae, code)
                 exprs.append(ae)
             # exprs.append(ast_expr)
-            codes.append(code)
+            # codes.append(code)
 
     search_code = open(os.path.join('codes/search', 'source1.py')).read()
 
@@ -262,6 +344,9 @@ if __name__ == "__main__":
     common_expr = ast_pm.get_common_expr(exprs)
     common_expr = ast_pm.make_simple_expr(common_expr)
     print(common_expr)
+
+    # print('DB: {}'.format(edb.db))
+    print(edb.query(['func', 'foo', [['return', ['+', 'bar', '?']]]], fuzzy=True))
 
     # TODO: add CodeItem class
 
